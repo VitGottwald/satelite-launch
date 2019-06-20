75fdc247e5c87b057db65cdff36db0a70efd5e05
Vit Gottwald <vit.gottwald@gmail.com>
2019-06-20 14:27:44 +0200

    Use @hot-loader/react-dom

--- src/TSApp.tsx
+++ src/TSApp.tsx
@@ -1,4 +1,4 @@
-import { hot } from 'react-hot-loader/root';
+import {hot} from 'react-hot-loader/root';
 import './App.css';
 
 import * as React from 'react';
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,9 @@ module.exports = {
   resolve: {
     // Add `.ts` and `.tsx` as a resolvable extension.
     extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
   },
   module: {
     rules: [
