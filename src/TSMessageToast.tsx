fbfb16b04ea6deb3b17995b90d0c6bf0922e25ec
Vit Gottwald <vit.gottwald@gmail.com>
2019-06-20 14:30:35 +0200

    Refmt .re / Prettier .tsx

--- src/TSMessageToast.tsx
+++ src/TSMessageToast.tsx
@@ -1,21 +1,23 @@
-import * as React from "react";
-import { hook as useMessageToast } from "./UseMessageToast.gen";
+import * as React from 'react';
+import {hook as useMessageToast} from './UseMessageToast.gen';
 
-
-const MessageToast = ({ header }: { header: string }) => {
+const MessageToast = ({header}: {header: string}) => {
   let [messages, addMessage] = useMessageToast();
   return (
-  <>
-    <div>
-      <h2>{header}</h2>
-      <button onClick={_ => addMessage((Date().toString()))}>
-        Add Message
-      </button>
-      <ul>
-        {messages.map(message => <li> {message.text} </li>)}
-          </ul>
-    </div>
-  </>
-)};
+    <>
+      <div>
+        <h2>{header}</h2>
+        <button onClick={_ => addMessage(Date().toString())}>
+          Add Message
+        </button>
+        <ul>
+          {messages.map(message => (
+            <li> {message.text} </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default MessageToast;
